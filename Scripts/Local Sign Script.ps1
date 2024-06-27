f81463a6f0a533bec9fa4ffd05de449fc74ff089
Bluecube-Kyle <Kyle.Baxter@Bluecube.legal>
2024-06-27 14:58:15 +0100

    Live revision

--- Scripts/Local Sign Script.ps1
+++ Scripts/Local Sign Script.ps1
@@ -1,16 +1,18 @@
+#Force Admin elevation prompt if not ran as administrator
 If(-not (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 	$arguments = "& '" + $myInvocation.MyCommand.Definition + "'"
 	Start-Process Powershell -Verb runAs -ArgumentList $arguments
 	exit
 }
 
+#Directory where scripts are stored
 $Path = "C:\Bluecube\Scripts\"
 $Scripts = Get-ChildItem $Path -Filter "*.ps1"
 
-Write-Output "Create and Sign or only Sign"
-$Signing = Read-Host -Prompt "Enter 1 for create and Sign. Else leave blank"
+#Look for existing Certificate and delete it
+Get-ChildItem "Cert:\LocalMachine\My" | Where-Object { $_.Subject -match 'ATA Authenticode' } | Remove-Item
 
-If($Signing -eq "1") {
+#Create new certificate
 $authenticode = New-SelfSignedCertificate -Subject "ATA Authenticode" -CertStoreLocation Cert:\LocalMachine\My -Type CodeSigningCert
 $rootStore = [System.Security.Cryptography.X509Certificates.X509Store]::new("Root","LocalMachine")
 $rootStore.Open("ReadWrite")
@@ -20,7 +22,7 @@ $publisherStore = [System.Security.Cryptography.X509Certificates.X509Store]::new
 $publisherStore.Open("ReadWrite")
 $publisherStore.Add($authenticode)
 $publisherStore.Close()
-}
 
+#Bind certificate to all .ps1 files in scripts folder
 $codeCertificate = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject -eq "CN=ATA Authenticode"}
 Foreach($Script in $Scripts) {Set-AuthenticodeSignature -FilePath "$Path$Script" -Certificate $codeCertificate -TimeStampServer "http://timestamp.digicert.com"}
--- Scripts/Script Updater.ps1
+++ Scripts/Script Updater.ps1
@@ -1,12 +1,22 @@
+#Force Admin elevation prompt if not ran as administrator
+If(-not (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+	$arguments = "& '" + $myInvocation.MyCommand.Definition + "'"
+	Start-Process Powershell -Verb runAs -ArgumentList $arguments
+	exit
+}
+
+#Create directory for scripts
 $ScriptPath = "C:\Bluecube\Scripts\"
 	If(!(Test-Path -PathType container $ScriptPath)) {New-Item -ItemType Directory -Path $ScriptPath}
 
+#Script names converted to variables
 $Seal = "Windows Sealing Script"
 $Patching = "Windows Patching Script"
 $Maintenance = "Windows Maintenance Script"
 $LocalCertSign = "Local Sign Script"
 $Updater = "Update Scripts"
 
+#Download Scripts from Github repo
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Bluecube-Kyle/VirtualDesktop-Scripts/main/Sealing/Windows%20Sealing%20Script.ps1" -OutFile $ScriptPath$Seal.ps1
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Bluecube-Kyle/VirtualDesktop-Scripts/main/Patching/Windows%20Patching%20Script.ps1" -OutFile $ScriptPath$Patching.ps1
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Bluecube-Kyle/VirtualDesktop-Scripts/main/Maintenance/Windows%20Maintenance%20Script.ps1" -OutFile $ScriptPath$Maintenance.ps1
@@ -14,8 +24,10 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Bluecube-Kyle/VirtualD
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Bluecube-Kyle/VirtualDesktop-Scripts/main/Scripts/Local%20Sign%20Script.ps1" -OutFile $ScriptPath$LocalCertSign.ps1
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Bluecube-Kyle/VirtualDesktop-Scripts/main/Scripts/Script%20Updater.ps1" -OutFile $ScriptPath$Updater.ps1
 
+#Acquire current users desktop path
 $Desktop = [Environment]::GetFolderPath("Desktop")
 
+#Create desktop shortcuts for all of the scripts
 $Scripts = "Windows Sealing Script,Windows Patching Script,Windows Maintenance Script,Local Sign Script,Update Scripts"
 $ScriptFile = $Scripts -Split ","
 Foreach($Script in $ScriptFile) { 
@@ -26,4 +38,5 @@ Foreach($Script in $ScriptFile) {
 	$shortcut.Save()
 }
 
+#Sign all of the scripts
 Powershell -F "$ScriptPath$LocalCertSign.ps1"
