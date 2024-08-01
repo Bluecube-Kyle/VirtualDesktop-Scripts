9e5c8c1278c78b896a9b0c7c5bb454ca9fce525f
Bluecube-Kyle <Kyle.Baxter@Bluecube.legal>
2024-08-01 10:52:06 +0100

    Update WU Browser Updates.ps1

--- Patching/WU Browser Updates.ps1
+++ Patching/WU Browser Updates.ps1
@@ -71,97 +71,174 @@ Start-Transcript -Append  -Path "$LogPath$Log - BrowserUpdates.log"
 Write-Output "====================---------- Start of Browser Patching ----------===================="
 Write-Output ""
 
-#--------------------Edge--------------------#
+
 Write-Progress -Activity "Browser Updates" -Status "Updating Browsers" -Id 1 -PercentComplete 0
 #Checking if Edge exists and clearing directory. (Running latest installer doesn't overwrite the existing exe's to latest version)
-$EdgeExists = Test-Path -Path "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+$Edge = Test-Path -Path "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 $Chrome = Test-Path -Path "C:\Program Files\Google\Chrome\Application\Chrome.exe"
 $Chrome64 = Test-Path -Path "C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe"
 $FireFox = Test-Path -Path "C:\Program Files\Mozilla Firefox"
-	If($EdgeExists -eq $true) {$TotalTasks += 3}
-	If(($Chrome -eq $true) -or ($Chrome64 -eq $true)) {$TotalTasks += 3}
-	If($FireFox -eq $true) {$TotalTasks += 3}
-
-	If($EdgeExists -eq $true) {
-		$Processes = Get-Process
-		If($Processes -match "msedge") {Stop-Process -Name MsEdge -Verbose -Force}
-		Start-Sleep 3
-		Remove-Item -Path 'C:\Program Files (x86)\Microsoft\Edge\Application\*.exe' -Force -Recurse
-
-		#Download Online Installer for latest release and run it
-		Write-Progress -Activity "Browser Updates" -Status "Downloading Latest Edge Version" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
-		Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2108834&Channel=Stable&language=en&brand=M100" -OutFile "$Installs\Edge.exe"
-		Write-Progress -Activity "Browser Updates" -Status "Installing Latest Edge Version" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
-		Start-Process "$Installs\Edge.exe" -Wait
-		Remove-Item -Path "$Installs\Edge.exe"
-		Write-Output "Edge Updated"
-
-		#Disable Automatic Updater services and tasks
-		Write-Progress -Activity "Browser Updates" -Status "Disabling Edge Services and Tasks" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
-		Start-Sleep 3
-		Stop-Process -Name MsEdge -Verbose
+$FireFox64 = Test-Path -Path "C:\Program Files (x86)\Mozilla Firefox"
+	If($Edge -eq $true) {$TotalTasks += 5}
+	If(($Chrome -eq $true) -or ($Chrome64 -eq $true)) {$TotalTasks += 5}
+	If(($FireFox -eq $true) -or ($FireFox64 -eq $true))  {$TotalTasks += 5}
+	
+	#--------------------Edge--------------------#
+	If($Edge -eq $true) {
+		Write-Progress -Activity "Browser Updates - Edge" -Status "Enabling Edge Services" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		Stop-Process -Name MsEdge -Verbose -Force -ErrorAction SilentlyContinue	
 		$Services = Get-Service
+		$EdgeServices = "edgeupdate,edgeupdatem,MicrosoftEdgeElevationService" -Split ","
+		$Matches = Select-String $EdgeServices -Input $Services -AllMatches | Foreach {$_.matches} | Select -Expand Value 
+			Foreach($Matches in $EdgeServices) {
+				If($Services -match $Matches) {
+				Set-Service $Matches -StartupType Manual | Restart-Service -Force
+				Write-Output "Startup of service $Matches set to Manual and Started"
+				} Else {Write-Output "$Matches not present"}
+			}
+		Write-Progress -Activity "Browser Updates - Edge" -Status "Running Edge Updater" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		Start-Process msEdge
+		Start-Sleep 5
+		$wshell = New-Object -ComObject wscript.shell;
+		$wshell.AppActivate("msEdge")
+		Start-Sleep 1
+		$wshell.SendKeys("Edge://help")
+		Start-Sleep 1
+		$wshell.SendKeys("{ENTER}")
+		Start-Sleep 10
+		
+		Write-Progress -Activity "Browser Updates - Edge" -Status "Waiting for Update completion" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		$Processes = Get-Process
+			If($Processes -Match "MicrosoftEdgeUpdate") {Wait-Process -Name MicrosoftEdgeUpdate}
+		Stop-Process -Name MsEdge -Verbose -Force
+		Start-Sleep 5
+
+		Write-Progress -Activity "Browser Updates - Edge" -Status "Applying Update" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		Start-Process msEdge
+		Start-Sleep 5
+		$wshell = New-Object -ComObject wscript.shell;
+		$wshell.AppActivate("msEdge")
+		Start-Sleep 1
+		$wshell.SendKeys("Edge://help")
+		Start-Sleep 1
+		$wshell.SendKeys("{ENTER}")
+		Start-Sleep 10
+
+		Write-Progress -Activity "Browser Updates - Edge" -Status "Disabling Services" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+			Foreach($Matches in $EdgeServices) {
+				If($Services -match $Matches) {
+				Set-Service $Matches -StartupType Disabled | Stop-Service -Force
+				Write-Output "Startup of service $Matches set to Disabled and Stopped"
+				} Else {Write-Output "$Matches not present"}
+			}
 		$ScheduledTask = Get-ScheduledTask
-			if($Services -match "edgeupdate") {Set-Service edgeupdate -StartupType Disabled -Passthru}
-			if($Services -match "edgeupdatem") {Set-Service edgeupdatem -StartupType Disabled -Passthru}
-			if($Services -match "MicrosoftEdgeElevationService") {Set-Service MicrosoftEdgeElevationService -StartupType Disabled -Passthru}
-			if($ScheduledTask -match "MicrosoftEdgeUpdateBrowserReplacement") {Disable-ScheduledTask -TaskName "MicrosoftEdgeUpdateBrowserReplacementTask"}
-			if($ScheduledTask -match "MicrosoftEdgeUpdateTaskMachineCore") {Disable-ScheduledTask -TaskName "MicrosoftEdgeUpdateTaskMachineCore"}
-			if($ScheduledTask -match "MicrosoftEdgeUpdateTaskMachineUA") {Disable-ScheduledTask -TaskName "MicrosoftEdgeUpdateTaskMachineUA"}
-	} 
-	else {Write-Output "Edge not present, skipping"}
+			If($ScheduledTask -match "MicrosoftEdgeUpdateBrowserReplacement") {Disable-ScheduledTask -TaskName "MicrosoftEdgeUpdateBrowserReplacementTask"}
+			If($ScheduledTask -match "MicrosoftEdgeUpdateTaskMachineCore") {Disable-ScheduledTask -TaskName "MicrosoftEdgeUpdateTaskMachineCore"}
+			If($ScheduledTask -match "MicrosoftEdgeUpdateTaskMachineUA") {Disable-ScheduledTask -TaskName "MicrosoftEdgeUpdateTaskMachineUA"}
+			
+		Stop-Process -Name MsEdge -Verbose -Force -ErrorAction SilentlyContinue	
+	} Else {Write-Output "Edge not present, skipping"}
 
-#--------------------Chrome--------------------#
+	#--------------------Chrome--------------------#
 	If(($Chrome -eq $true) -or ($Chrome64 -eq $true))  {
-	Write-Progress -Activity "Browser Updates" -Status "Downloading Latest Chrome Version" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
-	$ChromeUpdate = 'HKLM:\SOFTWARE\Policies\Google\Update'
-		If(!(Test-Path $ChromeUpdate)) {New-Item -Path $ChromeUpdate -Force}	
-	Set-ItemProperty -Path $ChromeUpdate -Name "UpdateDefault" -Value 1 -Type Dword -Force -PassThru
-	Invoke-WebRequest "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile "$Installs\Chrome.exe"
-	Write-Progress -Activity "Browser Updates" -Status "Installing Latest Chrome Version" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
-	Start-Process -FilePath "$Installs\Chrome.exe" -Args "/silent /install" -Verb RunAs -Wait
-	Remove-Item "$Installs\Chrome.exe"
-	Write-Output "Chrome Updated"
-
-	#Disable Automatic Updater services and tasks - Google adds an MSI code onto Scheduledtasks to try and prevent auto stopping the tasks. The following will acquire the task name and then wildcard for the MSIcode to disable it
-	Write-Progress -Activity "Browser Updates" -Status "Disabling Chrome Services and Tasks" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
-	Set-ItemProperty -Path $ChromeUpdate -Name "UpdateDefault" -Value 0 -Type Dword -Force -PassThru
-	$Services = Get-Service
-	$ScheduledTask = Get-ScheduledTask
-		If($Services -match "gupdate") {Set-Service gupdate -StartupType Disabled -PassThru}
-		If($Services -match "gupdatem") {Set-Service gupdatem -StartupType Disabled -PassThru}
+		Write-Progress -Activity "Browser Updates - Chrome" -Status "Enabling Services" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		Stop-Process -Name Chrome -Verbose -Force -ErrorAction SilentlyContinue	
+		$Services = Get-Service
+		If($Services -match "gupdate") {Set-Service gupdate -StartupType Manual | Start-Service}
+		If($Services -match "gupdatem") {Set-Service gupdatem -StartupType Manual | Start-Service}
 		$GoogleUpdaterService = (Get-Service -DisplayName "GoogleUpdater Service*").Name
 		$GoogleUpdaterIntService = (Get-Service -DisplayName "GoogleUpdater InternalService*").Name
-		Get-Service -DisplayName "GoogleUpdaterService*" | Set-Service -StartupType Disabled		
-		Get-Service -DisplayName "GoogleUpdaterInternalService*" | Set-Service -StartupType Disabled
-		Write-Output "Startup of service $GoogleUpdaterService set to Disabled"
-		Write-Output "Startup of service $GoogleUpdaterIntService set to Disabled"
-		If($Services -match "GoogleChromeElevationService") {Set-Service GoogleChromeElevationService -StartupType Disabled}
-		If($Tasks -match "GoogleUpdateTaskMachineUA*") {Get-ScheduledTask -TaskName "*GoogleUpdateTaskMachineUA*" | Disable-ScheduledTask}
-		If($Tasks -match "GoogleUpdateTaskMachineCore*") {Get-ScheduledTask -TaskName "*GoogleUpdateTaskMachineCore*" | Disable-ScheduledTask}
-		If($Tasks -match "GoogleUpdaterTaskSystem*") {Get-ScheduledTask -TaskName "*GoogleUpdaterTaskSystem*" | Disable-ScheduledTask}
-	}
-	else { Write-Output "Chrome Not installed. Skipping Update" }
+		Get-Service -DisplayName "GoogleUpdaterService*" | Set-Service -StartupType Manual | Start-Service	
+		Get-Service -DisplayName "GoogleUpdaterInternalService*" | Set-Service -StartupType Manual | Start-Service
+		Write-Output "Startup of service $GoogleUpdaterService set to Manual"
+		Write-Output "Startup of service $GoogleUpdaterIntService set to Manual"
+		If($Services -match "GoogleChromeElevationService") {Set-Service GoogleChromeElevationService -StartupType Manual | Start-Service}
 
-#--------------------Firefox--------------------#
-	If($FireFox -eq $true){
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Mozilla\FireFox" -Name "DisableAppUpdate" -Value 0 -Type Dword -Force -PassThru
-	#Downloads and runs the latest online installer
-	Write-Progress -Activity "Browser Updates" -Status "Downloading Latest Firefox Version" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
-	Invoke-WebRequest -Uri "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US" -OutFile "$Installs\FireFox.exe"
-	Write-Progress -Activity "Browser Updates" -Status "Installing Latest Firefox Version" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
-	Start-Process "$Installs\FireFox.exe" -ArgumentList "-ms -ma" -Wait
-	#Remove leftover installer
-	Remove-Item -Path "$Installs\FireFox.exe"
-	Write-Output "Firefox Updated"
-	
-	#Disable Automatic Updater services and tasks
-	Write-Progress -Activity "Browser Updates" -Status "Disabling FireFox Services and Tasks" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
-	$Services = Get-Service
-			if($Services -match "MozillaMaintenance") {Set-Service MozillaMaintenance -StartupType Disabled -Passthru}
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Mozilla\FireFox" -Name "DisableAppUpdate" -Value 1 -Type Dword -Force -PassThru
-	}
-	else {Write-Output "Firefox not present, skipping"}
+		Write-Progress -Activity "Browser Updates - Chrome" -Status "Running Updater" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		Start-Process Chrome
+		Start-Sleep 5
+		$wshell = New-Object -ComObject wscript.shell;
+		$wshell.AppActivate("Chrome")
+		Start-Sleep 1
+		$wshell.SendKeys("Chrome://help")
+		Start-Sleep 1
+		$wshell.SendKeys("{ENTER}")
+		Start-Sleep 10
+
+		Write-Progress -Activity "Browser Updates - Chrome" -Status "Waiting for Update completion" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		$Processes = Get-Process
+		If($Processes -Match "GoogleUpdate") {Wait-Process -Name GoogleUpdate}
+		Stop-Process -Name Chrome -Verbose -Force
+		Start-Sleep 5
+
+		Write-Progress -Activity "Browser Updates - Chrome" -Status "Applying Update" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		Start-Process Chrome
+		Start-Sleep 5
+		$wshell = New-Object -ComObject wscript.shell;
+		$wshell.AppActivate("Chrome")
+		Start-Sleep 1
+		$wshell.SendKeys("Chrome://help")
+		Start-Sleep 1
+		$wshell.SendKeys("{ENTER}")
+		Start-Sleep 10
+
+		Write-Progress -Activity "Browser Updates - Chrome" -Status "Disabling Services" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		$Services = Get-Service
+			If($Services -match "gupdate") {Set-Service gupdate -StartupType Disabled | Stop-Service -Force}
+			If($Services -match "gupdatem") {Set-Service gupdatem -StartupType Disabled | Stop-Service -Force}
+			$GoogleUpdaterService = (Get-Service -DisplayName "GoogleUpdater Service*").Name
+			$GoogleUpdaterIntService = (Get-Service -DisplayName "GoogleUpdater InternalService*").Name
+			Get-Service -DisplayName "GoogleUpdaterService*" | Set-Service -StartupType Disabled | Stop-Service	-Force
+			Get-Service -DisplayName "GoogleUpdaterInternalService*" | Set-Service -StartupType Disabled | Stop-Service -Force
+			Write-Output "Startup of service $GoogleUpdaterService set to Disabled"
+			Write-Output "Startup of service $GoogleUpdaterIntService set to Disabled"
+			If($Services -match "GoogleChromeElevationService") {Set-Service GoogleChromeElevationService -StartupType Disabled | Stop-Service -Force}
+			
+		Stop-Process -Name Chrome -Verbose -Force -ErrorAction SilentlyContinue	
+		} Else { Write-Output "Chrome Not installed. Skipping Update" }
+
+	#--------------------Firefox--------------------#
+	If(($FireFox -eq $true) -or ($FireFox64 -eq $true))  {
+		Write-Progress -Activity "Browser Updates - FireFox" -Status "Enabling Services" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		Stop-Process -Name FireFox -Verbose -Force -ErrorAction SilentlyContinue	
+		$Services = Get-Service
+			If($Services -match "MozillaMaintenance") {Set-Service MozillaMaintenance -StartupType Manual | Restart-Service}
+		Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Mozilla\FireFox" -Name "DisableAppUpdate" -Value 0 -Type Dword -Force -PassThru
+
+		Write-Progress -Activity "Browser Updates - FireFox" -Status "Running Updater" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		Start-Process FireFox
+		Start-Sleep 5
+		$wshell = New-Object -ComObject wscript.shell;
+		$wshell.AppActivate("FireFox")
+		Start-Sleep 1
+		$wshell.SendKeys("About:Settings")
+		Start-Sleep 1
+		$wshell.SendKeys("{ENTER}")
+		Start-Sleep 10
+
+		Write-Progress -Activity "Browser Updates - FireFox" -Status "Waiting for Update completion" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		$Processes = Get-Process
+			If($Processes -Match "Updater") {Wait-Process -Name Updater}
+		Stop-Process -Name FireFox -Verbose -Force -ErrorAction SilentlyContinue
+		Start-Sleep 5
+
+		Write-Progress -Activity "Browser Updates - FireFox" -Status "Applying Update" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		Start-Process FireFox
+		Start-Sleep 5
+		$wshell = New-Object -ComObject wscript.shell;
+		$wshell.AppActivate("FireFox")
+		Start-Sleep 1
+		$wshell.SendKeys("About:Settings")
+		Start-Sleep 1
+		$wshell.SendKeys("{ENTER}")
+		Start-Sleep 10
+
+		Write-Progress -Activity "Browser Updates - FireFox" -Status "Disabling Services" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+			If($Services -match "MozillaMaintenance") {Set-Service MozillaMaintenance -StartupType Disabled | Stop-Service -Force}
+		Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Mozilla\FireFox" -Name "DisableAppUpdate" -Value 1 -Type Dword -Force -PassThru	
+		
+		Stop-Process -Name FireFox -Verbose -Force -ErrorAction SilentlyContinue
+	} Else {Write-Output "Firefox not present, skipping"}
 
 Write-Output ""
 Write-Output "====================---------- End of Browser Patching ----------===================="
