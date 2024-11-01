37d88f1e71520b642868498dffa0db313ef6063a
Bluecube-Kyle <Kyle.Baxter@Bluecube.legal>
2024-11-01 13:52:57 +0000

    Update WU Office Updates.ps1

--- Patching/WU Office Updates.ps1
+++ Patching/WU Office Updates.ps1
@@ -66,32 +66,33 @@ Write-Output "====================---------- Start of Office Patching ----------
 Write-Output ""
 
 #Run Office Updater
-	If($Script:IncludeOfficeUpdates -eq "1") {
+If($Script:IncludeOfficeUpdates -eq "1") {
 	Write-Progress -Activity "Office Updates" -Status "Updating Office Applications" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
 	$OfficeUpdater = Test-Path -Path "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe"
-		If($OfficeUpdater -eq $true) {
+	If($OfficeUpdater -eq $true) {
 		Write-Output "Click to Run updater present. Beginning update"
 		If((Get-Service ClickToRunSvc | Select -Property Status) -notmatch "Running") {Start-Service ClicktoRunSvc}
 		Start-Process "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" -ArgumentList "/Update user forceappshutdown=true"
 		#Wait for Installer to report Up to date or updated - Required as C2RClients opens and closes multiple processes. -Wait only waits on the first to finish
 		#Checks processes status, Sleeps for 1s then rechecks process stats. Loops until condition is met of up to date message
-			While((($Process.MainWindowTitle -match "up to date!") -or ($Process.MainWindowTitle -match "Updates were installed")) -eq $false) {$Process = Get-Process ; Start-Sleep 5}
-			If($Process.MainWindowTitle -match "up to date!") {Write-Output "Office is already up to date"}
-			If($Process.MainWindowTitle -match "Updates were installed") {Write-Output "Office has been updated"}
+		While((($Process.MainWindowTitle -match "up to date!") -or ($Process.MainWindowTitle -match "Updates were installed")) -eq $false) {$Process = Get-Process ; Start-Sleep 5}
+		If($Process.MainWindowTitle -match "up to date!") {Write-Output "Office is already up to date"}
+		If($Process.MainWindowTitle -match "Updates were installed") {Write-Output "Office has been updated"}
 		
-			#Disable ScheduledTasks not required for non persistent image
-			Write-Progress -Activity "Office Updates" -Status "Disabling Scheduled Tasks" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
-			$Tasks = Get-ScheduledTask
-			#====================---------- Office Apps ----------====================#
-			If($Tasks -match "Office Automatic Updates 2.0") {Disable-ScheduledTask -TaskName "Office Automatic Updates 2.0" -TaskPath "\Microsoft\Office\"}
-			If($Tasks -match "Office ClickToRun Service Monitor") {Disable-ScheduledTask -TaskName "Office ClickToRun Service Monitor" -TaskPath "\Microsoft\Office\"}
-			If($Tasks -match "Office Feature Updates") {Disable-ScheduledTask -TaskName "Office Feature Updates" -TaskPath "\Microsoft\Office\"}
-			If($Tasks -match "Office Feature Updates Logon") {Disable-ScheduledTask -TaskName "Office Feature Updates Logon" -TaskPath "\Microsoft\Office\"}
-			If($Tasks -match "Office Serviceability Manager") {Disable-ScheduledTask -TaskName "Office Serviceability Manager" -TaskPath "\Microsoft\Office\"}
-			If($Tasks -match "OfficeTelemetryAgentFallBack2016") {Disable-ScheduledTask -TaskName "OfficeTelemetryAgentFallBack2016" -TaskPath "\Microsoft\Office\"}
-			If($Tasks -match "OfficeTelemetryAgentLogOn2016") {Disable-ScheduledTask -TaskName "OfficeTelemetryAgentLogOn2016" -TaskPath "\Microsoft\Office\"}
-		} Else {Write-Output "Office updater not present. Skipping"}
-	} Else {Write-Output "Office Updates are Disabled"}
+		#Disable ScheduledTasks not required for non persistent image
+		Write-Progress -Activity "Office Updates" -Status "Disabling Scheduled Tasks" -Id 1 -PercentComplete $PercentComplete ; $CurrentTask += 1 ; $PercentComplete = ($CurrentTask / $TotalTasks) * 100
+		$Tasks = Get-ScheduledTask
+		#====================---------- Office Apps ----------====================#
+		If($Tasks -match "Office Automatic Updates 2.0") {Disable-ScheduledTask -TaskName "Office Automatic Updates 2.0" -TaskPath "\Microsoft\Office\"}
+		If($Tasks -match "Office ClickToRun Service Monitor") {Disable-ScheduledTask -TaskName "Office ClickToRun Service Monitor" -TaskPath "\Microsoft\Office\"}
+		If($Tasks -match "Office Feature Updates") {Disable-ScheduledTask -TaskName "Office Feature Updates" -TaskPath "\Microsoft\Office\"}
+		If($Tasks -match "Office Feature Updates Logon") {Disable-ScheduledTask -TaskName "Office Feature Updates Logon" -TaskPath "\Microsoft\Office\"}
+		If($Tasks -match "Office Serviceability Manager") {Disable-ScheduledTask -TaskName "Office Serviceability Manager" -TaskPath "\Microsoft\Office\"}
+		If($Tasks -match "OfficeTelemetryAgentFallBack2016") {Disable-ScheduledTask -TaskName "OfficeTelemetryAgentFallBack2016" -TaskPath "\Microsoft\Office\"}
+		If($Tasks -match "OfficeTelemetryAgentLogOn2016") {Disable-ScheduledTask -TaskName "OfficeTelemetryAgentLogOn2016" -TaskPath "\Microsoft\Office\"}
+	} Else {Write-Output "Office updater not present. Skipping"}
+} Else {Write-Output "Office Updates are Disabled"}
+
 Write-Output ""
 Write-Output "====================---------- End of Office Patching ----------===================="
 Stop-Transcript
